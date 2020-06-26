7f97acae108d59a5cc34afa529676627fb6ca602
PamelaOlival <pamela.b.olival@gmail.com>
2020-06-26 12:40:13 -0300

    test

--- CRUD_Java/src/atividade3/JCliente.java
+++ CRUD_Java/src/atividade3/JCliente.java
@@ -493,7 +493,7 @@ public class JCliente extends javax.swing.JFrame {
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         // TODO add your handling code here:
         new JPedido().setVisible(true);
-        this.dispose();
+        this.dispose();//
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
